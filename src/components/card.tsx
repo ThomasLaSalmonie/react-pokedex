626539e847a6adb2996f11fe3ee244d9fd087321
thomas <tlasalmonie@gmail.com>
2024-06-20 11:41:03 -0400

    fix: Update css

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,8 +4,8 @@ import "./card.css"
 
 export default function PokemonCard(props: { pokemon: { name: string; image: string;}}) {
   return (
-    <Link href={`/pokemons/${props.pokemon.name}`} className="pokemon-card-link">
-      <div className="pokemon-card">
+    <Link href={`/pokemons/${props.pokemon.name}`} className="pokemon-card pokemon-card-link">
+      <div>
         <div>
           <Image width="200" height="200" src={props.pokemon.image} alt={props.pokemon.name} />
           <h2>{props.pokemon.name}</h2>
